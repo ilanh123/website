616db478232fc604d6fba199ea30884caea244fa
ilanh123 <112961571+ilanh123@users.noreply.github.com>
2023-05-29 20:52:18 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,66 @@
+<head>
+  <title>Home Page</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap"
+  rel="stylesheet"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<nav class="navbar">
+  <ul class= "navbar-nav">
+    <li class="logo">
+      <a href="#" class="nav-link">
+          <span class="link-text logo-text">ilan hamond</span>
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fad"
+          data-icon="angle-double-right"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="svg-inline--fa fa-angle-double-right fa-w-12a fa-2x"
+          width="80"
+        >
+          <g class="fa-group">
+            <path
+              fill="currentColor"
+              d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+              class="fa-secondary"
+            ></path>
+            <path
+              fill="currentColor"
+              d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+              class="fa-primary"
+            ></path>
+          </g>
+        </svg>
+      </a>
+    </li>
+
+    <li class= "nav-item">
+      <a href="#" class="nav-link">
+        <span class="fa-solid fa-house-circle-check fa-3x"></span>
+        <span class="link-text">Home</span>
+      </a>
+    </li>
+
+    <li class= "nav-item">
+      <a href="#" class="nav-link">
+        <span class="fa-solid fa-pen-clip fa-3x"></span>
+        <span class="link-text">About</span>
+      </a>
+    </li>
+
+    <li class= "nav-item">
+      <a href="#" class="nav-link"> 
+        <span class="fa-solid fa-envelope-open-text fa-3x"></span>
+        <span class="link-text">Contact</span>
+      </a>
+    </li>
+
+  </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+  $title = 'Home';
+  $page = 'home';
+  include_once('header.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<main>
+  <body>
+    <div class= "container">
+      <div id= "hero">
+        <div id="box-transition">
+            <h1>My name is Ilan Hamond</h1>
+          </div>
+          <p>& welcome to my page.</p>
+      </div> <!-- This is the ending to the HERO div-->
+    </div>
+  </body>
+</main>
+</html>
